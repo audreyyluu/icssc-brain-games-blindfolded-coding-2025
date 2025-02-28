0936c1cc055bcb3998d894654c8078f861b53644
Lunar-Y <christopheryoungsy@gmail.com>
2025-02-27 22:30:56 -0800

    Update problem4b.py
    Changed the way the return is formatted.
    Added a way for players to run their own code.

--- problem4b.py
+++ problem4b.py
@@ -1,4 +1,12 @@
 def problem4b(input):
-    #Code your function here
+    # Code your function here
 
-    return #Please return your answer
+    output = False # Please set your answer to this
+    return {"Input": input, "Output": output} # DO NOT MODIFY THIS LINE
+
+
+
+if __name__ == "__main__":
+    # Feel free to add additional tests here
+    sample_input = [[1/2, 3/8, 1/4, 1/8]]
+    print(problem4b(sample_input)) # Output should be 25
