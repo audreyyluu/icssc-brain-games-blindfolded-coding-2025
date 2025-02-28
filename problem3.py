b30bf5a9cddea0a1caa3fd31bec53cbde5867ce7
Lunar-Y <christopheryoungsy@gmail.com>
2025-02-27 22:30:51 -0800

    Update problem3.py
    Changed the way the return is formatted.
    Added a way for players to run their own code.

--- problem3.py
+++ problem3.py
@@ -1,4 +1,15 @@
 def problem3(input):
-    #Code your function here
+    # Code your function here
     
-    return #Please return your answer
+    
+    output = False # Please set your answer to this
+    return {"Input": input, "Output": output} # DO NOT MODIFY THIS LINE
+
+
+
+if __name__ == "__main__":
+    # Feel free to add additional tests here
+    sample_input = [1/8, 1/4, 1/8, 1/2, 1/4, 1/4, 1/8]
+    print(problem3(sample_input)) # Output should be 4
+
+    
