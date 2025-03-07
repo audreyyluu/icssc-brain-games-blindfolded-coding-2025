7d9a82740ba1af7c2ceafe24c464ab365223425d
Anthony Tam <149894302+anthonytm2@users.noreply.github.com>
2025-03-06 17:47:28 -0800

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,9 @@
+def problem2(input):
+    # Code your function here
+
+    output = False # Please set your answer to this
+    return {"Input": input, "Output": output} # DO NOT MODIFY THIS LINE
+
+if __name__ == '__main__':
+    sample_input = [(1,7), (3,4), (2,5)]
+    print(problem3(sample_input)) # should be [(3,4), (2,5), (1,7)]
