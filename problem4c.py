81461b20843adbb9946fac208e9b160489d6b14a
jyaldere <juliannayalderete@gmail.com>
2025-03-23 14:55:53 -0700

    Update problem4c.py

--- problem4c.py
+++ problem4c.py
@@ -13,7 +13,7 @@ def problem4c(parties: list, total_cost: list) -> list:
                             **Assume party discount from problem4b has already been calculated**
                             
     Returns: 
-        output: A list of dictionaries representing the the cost for every person in each party.
+        output: A dictionary representing the the cost for every person in each party.
                 The key is the discounted day of the week and the value is a nested list (each inner
                 list representing a party) with the calculated individual costs. 
     """
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     #Test your code here
 
     print(problem4c([[1/2, 1/8, 3/4, 5/8], [1/2, 1, 1/8]], [11, 13])) 
-    # Output:[
-    # {'Sunday': [[2.06, 0.52, 3.09, 2.58], [3.0, 6.0, 0.75]]},
-    # {'Wednesday': [[2.34, 0.58, 3.51, 2.92], [3.4, 6.8, 0.85]]}, 
-    # {'Friday': [[2.2, 0.55, 3.3, 2.75], [3.2, 6.4, 0.8]]} ]
+    # Output:
+    # {'Sunday': [[2.06, 0.52, 3.09, 2.58], [3.0, 6.0, 0.75]],
+    # 'Wednesday': [[2.34, 0.58, 3.51, 2.92], [3.4, 6.8, 0.85]], 
+    # 'Friday': [[2.2, 0.55, 3.3, 2.75], [3.2, 6.4, 0.8]]} 
         
