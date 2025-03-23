600a7fd9b820dfedb93749db9bc94ec51bdbaaa9
Anthony Tam <149894302+anthonytm2@users.noreply.github.com>
2025-03-23 01:44:23 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,8 +1,24 @@
-def problem2(input):
+import math
+
+def problem2(locations : list[tuple]):
+    """
+    This function calculates the most efficient delivery route 
+    for three tables' coordinates.
+    
+    Args:
+        locations (list[tuples]): A list of three tuples, each tuple is the
+                      Cartesian coordinates (x and y) of a table.
+    
+    Returns:
+        output (list[tuples]): The reordered list from input with the 
+                               tuples in the order to be traversed.
+    """
     # Code your function here
 
-    output = False # Please set your answer to this
-    return {"Input": input, "Output": output} # DO NOT MODIFY THIS LINE
+
+    
+    output = [] # Please set your answer to this
+    return {"Input": locations, "Output": output} # DO NOT MODIFY THIS LINE
 
 if __name__ == '__main__':
     sample_input = [(1,7), (3,4), (2,5)]
